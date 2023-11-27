cffe7b0307caba35747d58fa44e4db9dbfab6cdd
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-11-27 19:54:23 +0100

    [PFM11-2] Created FilterType and helper free-functions

---
+++ Source/utils/FilterType.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <juce_core/juce_core.h>
+
+namespace FilterInfo
+{
+	enum class FilterType
+	{
+		FIRST_ORDER_LOWPASS,
+		FIRST_ORDER_HIGHPASS,
+		FIRST_ORDER_ALLPASS,
+		LOWPASS,
+		HIGHPASS,
+		BANDPASS,
+		NOTCH,
+		ALLPASS,
+		LOWSHELF,
+		HIGHSHELF,
+		PEAKFILTER,
+	};
+
+	juce::String getFilterTypeName(FilterType filterType);
+	juce::StringArray getFilterTypeNames();
+} // namespace FilterInfo
