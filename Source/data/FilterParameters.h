d446b99a97d207ef3a040f7b46149e3b52cbd1a8
Fabrizio Perria <fabrizio.perria@gmail.com>
2023-11-29 10:38:22 +0100

    Collapse filter parameters struct declarations in a single file

--- Source/data/FilterParameters.h
+++ Source/data/FilterParameters.h
@@ -1,10 +1,24 @@
 #pragma once
 
-#include "data/FilterParametersBase.h"
 #include "utils/FilterType.h"
+
+struct FilterParametersBase
+{
+    float frequency = 440.0f;
+    bool bypassed = false;
+    float quality = 1.0f;
+    double sampleRate = 44100.0;
+};
 
 struct FilterParameters : public FilterParametersBase
 {
     FilterInfo::FilterType type = FilterInfo::FilterType::ALLPASS;
     float gain = 0.0f;
 };
+
+struct HighCutLowCutParameters : public FilterParametersBase
+{
+    int order = 1;
+    bool isLowCut = false;
+};
+
--- Source/data/HighCutLowCutParameters.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "data/FilterParametersBase.h"
-
-struct HighCutLowCutParameters : public FilterParametersBase
-{
-    int order = 1;
-    bool isLowCut = false;
-};
