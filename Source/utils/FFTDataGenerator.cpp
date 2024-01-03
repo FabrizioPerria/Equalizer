bd5b095b19dc581633a2d40d68c0eaf1a36dfab2
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-01-03 22:01:19 +0100

    [PFM11-32] Properly clean fftData vector

--- Source/utils/FFTDataGenerator.cpp
+++ Source/utils/FFTDataGenerator.cpp
@@ -5,7 +5,7 @@ void FFTDataGenerator::produceFFTDataForRendering (const juce::AudioBuffer<float
 {
     auto fftSize = getFFTSize();
 
-    fftData.assign (fftSize, 0.0f);
+    fftData.resize (2 * fftSize, 0);
 
     auto reader = audioData.getReadPointer (0);
     std::copy (reader, reader + fftSize, fftData.begin());
@@ -31,7 +31,8 @@ void FFTDataGenerator::changeOrder (FFTOrder newOrder)
     order = newOrder;
     window = std::make_unique<juce::dsp::WindowingFunction<float>> (fftSize, juce::dsp::WindowingFunction<float>::blackmanHarris);
     forwardFFT = std::make_unique<juce::dsp::FFT> (static_cast<int> (order));
-    std::fill (fftData.begin(), fftData.end(), 0.0f);
+    fftData.clear();
+    fftData.resize (2 * fftSize, 0);
     fftDataFifo.prepare (2 * fftSize);
 }
 
