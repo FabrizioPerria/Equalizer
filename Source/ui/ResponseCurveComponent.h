5e5c7f2c1eb1e72c6420fcad9753397194cdf287
Fabrizio Perria <fabrizio.perria@gmail.com>
2024-01-15 19:12:21 +0100

    [PFM11-42] Implement response curve

---
+++ Source/ui/ResponseCurveComponent.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "PluginEditor.h"
+#include "PluginProcessor.h"
+#include "ui/AnalyzerBase.h"
+#include <JuceHeader.h>
+
+struct ResponseCurveComponent : AnalyzerBase
+{
+    ResponseCurveComponent (double sr, juce::AudioProcessorValueTreeState& apvts);
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+
+private:
+    juce::AudioProcessorValueTreeState* apvts;
+    double sampleRate;
+    /* std::unique_ptr<AllParamsListener> allParamsListener; */
+
+    EqualizerAudioProcessor::MonoChain leftChain, rightChain;
+    juce::Path leftResponseCurve, rightResponseCurve;
+
+    void refreshParams();
+    void buildNewResponseCurves();
+    void updateChainParameters();
+    void buildNewResponseCurve (std::vector<float>& path, EqualizerAudioProcessor::MonoChain& chain);
+    void createResponseCurve (juce::Path& path, const std::vector<float>& data);
+};
